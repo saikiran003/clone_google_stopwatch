159f13e1430d5e825cd15779ac8bfae81c83bba7
saikiran003 <kiranarishanpalli@gmail.com>
2022-04-17 15:48:26 +0530

    stopwatch completed

---
+++ my-app/src/App.js
@@ -0,0 +1,67 @@
+import "./App.css";
+import BtnComponent from "./Components/BtnComponent";
+import DisplayComponent from "./Components/DisplayComponent";
+import React from "react";
+
+function App() {
+  const [time, setTime] = React.useState({ ms: 0, s: 0, m: 0, h: 0 });
+  const [interv, setInterv] = React.useState();
+  const [status, setStatus] = React.useState(0);
+
+  const start = () => {
+    run();
+    setStatus(1);
+    setInterv(setInterval(run, 10));
+  };
+
+  var updatedMs = time.ms,
+    updatedS = time.s,
+    updatedM = time.m,
+    updatedH = time.h;
+
+  const run = () => {
+    if (updatedM == 60) {
+      updatedH++;
+      updatedM = 0;
+    }
+    if (updatedS === 60) {
+      updatedM++;
+      updatedS = 0;
+    }
+    if (updatedMs === 100) {
+      updatedS++;
+      updatedMs = 0;
+    }
+    updatedMs++;
+    return setTime({ ms: updatedMs, s: updatedS, m: updatedM, h: updatedH });
+  };
+
+  const stop = () => {
+    clearInterval(interv);
+    setStatus(2);
+  };
+  const reset = () => {
+    clearInterval(interv);
+    setStatus(0);
+    setTime({ ms: 0, s: 0, m: 0, h: 0 });
+  };
+  const resume = () => start();
+  return (
+    <div className="main-section">
+      <div className="clock-holder">
+        <div className="stopwatch">
+          <DisplayComponent time={time} />
+          <BtnComponent
+            start={start}
+            stop={stop}
+            resume={resume}
+            reset={reset}
+            status={status}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
